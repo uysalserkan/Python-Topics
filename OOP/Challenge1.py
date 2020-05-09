31b46fec08115d69585d367050c9b43dbc20c6a4
uysalserkan <serqkanseko@gmail.com>
2020-05-09 21:53:15 +0300

    challenges

---
+++ OOP/Challenge1.py
@@ -0,0 +1,43 @@
+# Challenge 1
+class Point:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def sqSum(self):
+        return self.x**2 + self.y**2 + self.z**2
+
+
+# Challenge 2
+class Student:
+    def __init__(self, name, phy, chem, bio):
+        self.name = name
+        self.phy = phy
+        self.chem = chem
+        self.bio = bio
+
+    def totalObtained(self):
+        return self.phy + self.chem + self.bio
+
+    def Percentage(self):
+        return (self.totalObtained()) / 3
+
+
+# Challenge 3
+class Calculator:
+    def __init__(self, n1, n2):
+        self.num1 = n1
+        self.num2 = n2
+
+    def add(self):
+        return self.num1 + self.num2
+
+    def subtract(self):
+        return self.num2 - self.num1
+
+    def multiply(self):
+        return self.num1 * self.num2
+
+    def divide(self):
+        return self.num2 / self.num1
