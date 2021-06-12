63abeecff03909d443d18dd21a1ca53436988685
uysalserkan <uysalserkan08@gmail.com>
2021-06-12 14:22:41 +0300

    Project done. Creating page, update page, delete page finished.

--- Django Apps/crud_operations/main/urls.py
+++ Django Apps/crud_operations/main/urls.py
@@ -1,9 +1,11 @@
-from main.views import employee_form, employee_list
+from main.views import employee_delete, employee_form, employee_list
 from django.urls import path
 from .models import *
 
 urlpatterns = [
-    path('', employee_form),
-    path('list/', employee_list),
-    path('form/', employee_form),
+    path('', employee_form, name='home'),
+    path('list/', employee_list, name='list'),
+    path('form/', employee_form, name="form"),
+    path('update/<int:id>', employee_form, name='update'),
+    path('delete/<int:id>', employee_delete, name='delete'),
 ]
--- Django Apps/crud_operations/main/views.py
+++ Django Apps/crud_operations/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render, HttpResponse
 from .forms import EmployeeForm
+from .models import Employee
 
 # Create your views here.
 
@@ -9,15 +10,19 @@ def employee_list(request):
         request=request,
         template_name='src/emp_list.html',
         context={
-
+            'employees': Employee.objects.all(),
         }
     )
 
 
-def employee_form(request):
+def employee_form(request, id=0):
 
     if request.method == 'GET':
-        form = EmployeeForm
+        if id == 0:
+            form = EmployeeForm
+        else:
+            emp = Employee.objects.get(pk=id)
+            form = EmployeeForm(instance=emp)
 
         return render(
             request=request,
@@ -28,12 +33,18 @@ def employee_form(request):
         )
 
     else:
-        form = EmployeeForm(request.POST)
+        if id == 0:
+            form = EmployeeForm(request.POST)
+        else:
+            emp = Employee.objects.get(pk=id)
+            form = EmployeeForm(request.POST, instance=emp)
         if form.is_valid:
             form.save()
 
         return redirect('list/')
 
 
-def employee_delete(request):
-    pass
+def employee_delete(request, id):
+    emp = Employee.objects.get(pk=id)
+    emp.delete()
+    return redirect('list/')
