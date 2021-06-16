f8187714509668c9b30fa792d91ba664b15d9d9d
uysalserkan <uysalserkan08@gmail.com>
2021-06-16 12:47:03 +0300

    Tek makale için GET, PUT, DELETE response'leri yaratıldı.

--- Django Apps/haber_djangorest/main/api/urls.py
+++ Django Apps/haber_djangorest/main/api/urls.py
@@ -3,4 +3,5 @@ from main.api import views as api_views
 
 urlpatterns = [
     path("makaleler", api_views.makale_list_create_api_view, name="makale-listesi"),
+    path("makaleler/<int:id>", api_views.makale_detail_api_view, name="makale-detail"),
 ]
--- Django Apps/haber_djangorest/main/api/views.py
+++ Django Apps/haber_djangorest/main/api/views.py
@@ -26,3 +26,55 @@ def makale_list_create_api_view(request):
             serialized.save()
             return Response(serialized.data, status=status.HTTP_201_CREATED)
         return response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def makale_detail_api_view(request, id):
+    """
+    Request ile eğer bir makale var ise (aktif veya değil fark etmez)
+    bu makaleye erişip üzerinde değişiklikler yapabilmeliyiz.
+    """
+
+    try:
+        makale_inst = Makale.objects.get(pk=id)
+    except Makale.DoesNotExist:
+        return Response(
+            {
+                "errors": {
+                    "code": 404,
+                    "message": f"Böyle bir id ({id}) ile ilgili makale bulunamadı.. Lütfen kontrol edip tekrar deneyiniz..",
+                }
+            },
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
+    if request.method == "GET":
+        serialized = MakaleSerializer(makale_inst)
+        return Response(serialized.data)
+
+    elif request.method == "PUT":
+        serialized = MakaleSerializer(makale_inst, data=request.data)
+        if serialized.is_valid():
+            serialized.save()
+            return Response(serialized.data)
+        return Response(
+            {
+                "errors": {
+                    "code": 400,
+                    "message": f"Değiştirmek istediğiniz {id}'li makalede bir hata meydana geldi. Lütfen girdilerinizi kontrol edip tekrar deneyiniz..",
+                }
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    elif request.method == "DELETE":
+        makale_inst.delete()
+        return Response(
+            {
+                "complete": {
+                    "code": 202,
+                    "message": f"{id} id numarasına sahip makale database üzerinden kaldırıldı..",
+                }
+            },
+            status=status.HTTP_202_ACCEPTED,
+        )
